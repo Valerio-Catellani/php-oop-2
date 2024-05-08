d05a8bcd6d8475712b7f81a2583c7b4c02cf6644
Valerio-Catellani <valeriocatellani@gmail.com>
2024-05-08 19:57:51 +0200

    created class Prodotto

---
+++ Models/Prodotti.php
@@ -0,0 +1,42 @@
+<?php
+include __DIR__ . "/Category.php";
+class Prodotto
+{
+    protected $id;
+    protected $img;
+    protected $name;
+    protected $prezzo;
+    public Category $animale;
+    protected $valutazione;
+
+    public function __construct($id, $img, $name, $prezzo, $animale, $valutazione)
+    {
+        $this->id = $id;
+        $this->img = $img;
+        $this->name = $name;
+        $this->prezzo = $prezzo;
+        $this->animale = $animale;
+        $this->valutazione = $valutazione;
+    }
+
+
+    public static function fetchAll($path)
+    {
+        $data = file_get_contents(__DIR__ . '/../resurces/' . $path . '.json');
+        $dataToArray = json_decode($data, true);
+        $arrayAllCategories = Category::getAllCategories();
+        var_dump($arrayAllCategories['0']);
+        $elements = [];
+
+        foreach ($dataToArray as $item) {
+            $category = null;
+            foreach ($arrayAllCategories as $categoriaSingola) {
+                if ($categoriaSingola->name == $item["animale"]) {
+                    $category = $categoriaSingola;
+                }
+            }
+            $elements[] = new Prodotto($item['id'], $item['img'], $item['name'], $item['prezzo'], $category, $item['valutazione']);
+        }
+        return $elements;
+    }
+}
