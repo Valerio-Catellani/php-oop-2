61794a8ced96ce219bc5c69d84b443b3d40a48e1
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-05-09 10:18:02 +0200

    cose in comune

---
+++ Models/Accessori.php
@@ -0,0 +1,8 @@
+    <!--"tipo": "accessorio", ALL
+    "tipoDiAccessorio": "cucce",
+
+
+    ALL
+    "marca": "Cane Sportivo",
+    "descrizione": "Cuccia morbida e confortevole per il riposo del tuo cane",
+    "disponibilità": "disponibile" -->
